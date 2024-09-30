cb592bcf764399574000d0a7d4abd670ecbcbd6e
Toyehamzat <anibiseun@gmail.com>
2024-09-30 12:02:33 +0100

    Refactor dashboard layout to include metadata and multi-user login system

---
+++ app/(dahsboard)/editor-dashboard/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function AdminDashboard() {
+  return (
+    <div className="p-8">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Editor Dashboard</CardTitle>
+          <CardDescription>
+            Welcome to your editor control panel.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="mb-4">
+            Here you can manage users, view system statistics, and perform other
+            editors tasks.
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ app/(dahsboard)/viewer-dashboard/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function AdminDashboard() {
+  return (
+    <div className="p-8">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Viewer Dashboard</CardTitle>
+          <CardDescription>
+            Welcome to your viewer control panel.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="mb-4">
+            Here you can manage users, view system statistics, and perform other
+            viewer tasks.
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
